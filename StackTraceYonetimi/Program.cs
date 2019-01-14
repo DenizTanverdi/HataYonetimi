b8a58f320a3edecf52114c8d092c3196a6a024a3
semvs <semvs@ayvansaray.edu.tr>
2019-01-14 14:35:56 +0300

    Path.GetFileName

--- StackTraceYonetimi/Program.cs
+++ StackTraceYonetimi/Program.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using Dapper;
+using System.IO;
+
 namespace StackTraceYonetimi
 {
+
     class Program
     {
+        static SqlConnection con;
         static void Main(string[] args)
         {
             uygulama();
             Console.ReadKey();
 
         }
-        public static void uygulama() {
+        public static void uygulama()
+        {
 
 
             try
@@ -23,8 +30,9 @@ namespace StackTraceYonetimi
                 int i = Convert.ToInt32(str);
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                con = new SqlConnection("Data Source=SEM-BILGISAYAR;Initial Catalog=test;Persist Security Info=True;User ID=test2;Password=test2");
                 // hatanın satır numarasına ulasmak için true değeri verilir
                 StackTrace stack = new StackTrace(true);
 
@@ -32,12 +40,21 @@ namespace StackTraceYonetimi
                 {
                     if (!string.IsNullOrEmpty(frame.GetFileName()))
                     {
-                        Console.WriteLine($"Dosya Adi : {frame.GetFileName()}");
+                        string gelen = frame.GetFileName().ToString();
+
+                        //Path.GetFileName() adres sadelestiriyo
+
+                        con.Execute("Insert Into HataLoglari(DosyaAdi,MethodAdi,LineNumber,ColumnNumber,message)Values(@DosyaAdi,@MathodAdi,@LineNumber,@ColumnNumber,@message)", new[] { new { @DosyaAdi =Path.GetFileName(frame.GetFileName()),//.Split('\\').Last()
+                            @MathodAdi = frame.GetMethod().ToString() ,
+                            @LineNumber =frame.GetFileLineNumber().ToString() ,
+                            @ColumnNumber = frame.GetFileColumnNumber().ToString(),
+                            @message = ex.Message} });
+                        Console.WriteLine($"Dosya Adi : {Path.GetFileName(frame.GetFileName())}");
                         Console.WriteLine($"Line Number : {frame.GetFileLineNumber()}");
-                        Console.WriteLine($"Column Number : {frame.GetFileColumnNumber()}");
+                        Console.WriteLine($"Column Number : {frame.GetFileLineNumber()}");
                         Console.WriteLine($"Method Name : {frame.GetMethod()}");
-
-
+                       
+                        //header.Trim( new Char[] { ' ', '*', '.' } )
 
                     }
                 }
